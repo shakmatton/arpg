bd3557e8a78c6b642523b763bd99d95e6a782320
shakmatton <118413324+shakmatton@users.noreply.github.com>
2024-01-22 18:39:49 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // cb.scene.rotation.set(0, 9.5, 0)
         
 
-        cb.scene.userData.clickable = true;          // We'll do this to all models which we want to capture an event (like the event 'click', further below).
+        cb.scene.userData.clickable = true;          
 
 
         const cbAnchor = mindarThree.addAnchor(0)
@@ -70,21 +70,30 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
         const footnote = new CSS3DObject(document.querySelector("#ar-footnote"))
-
-        const footnoteAnchor = mindarThree.addCSSAnchor(0)
-        footnoteAnchor.group.add(footnote)
-
-
         const headingARG = new CSS3DObject(document.querySelector("#ar-heading-arg"))
         const headingUSA = new CSS3DObject(document.querySelector("#ar-heading-usa"))
 
+        const footnoteAnchor = mindarThree.addCSSAnchor(0)
         const headingARGAnchor = mindarThree.addCSSAnchor(0)
         const headingUSAAnchor = mindarThree.addCSSAnchor(0)
 
+        footnoteAnchor.group.add(footnote)
         headingARGAnchor.group.add(headingARG)
         headingUSAAnchor.group.add(headingUSA)
 
+
         
+/*      onTargetFound usage example:
+
+        anchor.onTargetFound = () => {
+            audio.play()   // the song is played when the raccoon is found (instead of when the app starts)
+        }
+
+        anchor.onTargetLost = () => {
+            audio.pause()  // pause the song when the raccoon is lost.
+        }
+*/
+
 
         // In your main.js, find the first image element
         // const arImage = document.getElementById("ar-image");
@@ -95,10 +104,8 @@ document.addEventListener("DOMContentLoaded", () => {
         /*   >>>>>>>   CLOCKS & RENDERS  <<<<<<   */
 
         
-        let cbRotation = 0;                                      // Initial rotation
-
-
-
+        let cbRotation = 0                                      // Initial countryball rotation
+        let cbTurn = 0                                          // Counter will assist CSS flags switching
 
 
         document.body.addEventListener("pointerdown", (e) => {
@@ -121,22 +128,18 @@ document.addEventListener("DOMContentLoaded", () => {
             
                 if (o.userData.clickable) {                  
                     if (o === cb.scene) {                 
-                        cbRotation += Math.PI;                               // Rotate 180 degrees
-                        cb.scene.rotation.y = cbRotation;                            
+                        cbRotation += Math.PI;                               // Countryball must now rotate 180 degrees
+                        cb.scene.rotation.y = cbRotation;                    // Executes 180 degrees rotation    
+                        cbTurn++                                             // Updates number of countryball turns
                     }
                 }
             }
         });
 
 
-
-
-
-
         const clock = new THREE.Clock()
 
         await mindarThree.start()
-
 
 
         renderer.setAnimationLoop(() => {
@@ -150,24 +153,32 @@ document.addEventListener("DOMContentLoaded", () => {
             cb.scene.position.x = Math.cos(cbAngle) * 0.4
             cb.scene.position.y = Math.sin(cbAngle) * 0.4
             
-            /*
-            const ghost1Angle = elapsedTime * 0.5               // fixed radius
+            /* const ghost1Angle = elapsedTime * 0.5               // fixed radius
+               ghost1.position.x = Math.cos(ghost1Angle) * 4    ghost1.position.z = Math.sin(ghost1Angle) * 4       ghost1.position.y = Math.sin(elapsedTime * 3)    */ 
 
-            ghost1.position.x = Math.cos(ghost1Angle) * 4
-            ghost1.position.z = Math.sin(ghost1Angle) * 4
-            ghost1.position.y = Math.sin(elapsedTime * 3)
-
-            */ 
-
-            cb.scene.rotation.y = cbRotation            
+            // cb.scene.rotation.y = cbRotation            
             
+
+            if (cbTurn % 2 == 0) {                                  // this means the countryball will switch flags each time the number of turns is even or odd.   
+                headingARG.element.style.visibility = "visible";    
+                headingUSA.element.style.visibility = "hidden";    
+                headingUSA.position.y = 99999;                      // this solves the z-buffer conflict between both css elements
+                headingARG.position.y = 0;                          // when a css position is set to 0, it is displayed normally on screen.
+            } else {
+                headingUSA.element.style.visibility = "visible";   
+                headingARG.element.style.visibility = "hidden";     
+                headingARG.position.y = 99999;                      // when a css position is set to 99999, it is displayed far away (up and out of the camera display range). 
+                headingUSA.position.y = 0;     
+            }
+
+/*
             const angleThreshold = Math.PI;                 // Threshold for switching flags
             const isArgentinaFlagVisible = cbRotation < angleThreshold;
     
             headingARG.element.style.visibility = isArgentinaFlagVisible ? "visible" : "hidden";
             headingUSA.element.style.visibility = !isArgentinaFlagVisible ? "visible" : "hidden";
 
-
+*/
 
             renderer.render(scene,camera)
             cssRenderer.render(cssScene, camera)
@@ -176,17 +187,9 @@ document.addEventListener("DOMContentLoaded", () => {
             //     setTimeout(() => {
             //       arImage.classList.add("faded"); // Faded class adds "fade-in" to avoid redundant animation
             //     }, 500); // Adjust delay as needed
-            //   }
+            //   }            
 
-
-            // mindarThree.on("end", () => {
-            //     headingARG.element.style.visibility = "hidden";
-            //     headingUSA.element.style.visibility = "hidden";
-            //     // ... other cleanup
-            //   });
-
-        })
-    
+        })    
     }
 
     start()
